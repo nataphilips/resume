145cb5f1c92ef68e7950fe7e31b991ef754911e5
nataphilips <fnd.natalia@gmail.com>
2020-03-13 05:23:43 +0300

    Updates

---
+++ precache-manifest.6bc1e06b32defb2d645670bd6c082f40.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "624dc4fa7b6217f77b21a2181975c8b3",
+    "url": "/profile-page/index.html"
+  },
+  {
+    "revision": "b5e0472d7bce1bd1f813",
+    "url": "/profile-page/static/js/2.cc049401.chunk.js"
+  },
+  {
+    "revision": "727c110e637187b6d5f6",
+    "url": "/profile-page/static/js/main.9e68bc7f.chunk.js"
+  },
+  {
+    "revision": "81bab1a4e2d55924c570",
+    "url": "/profile-page/static/js/runtime~main.088de7ab.js"
+  }
+]);
